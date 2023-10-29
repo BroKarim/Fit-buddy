26ce9f16369d89ee1208853258f68b6e2dac489f
kiramDZ <hilmi0@mhs.unsyiah.ac.id>
2023-10-29 17:05:56 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import Hero from './components/Hero';
+import Header from './components/Header'
+
+
+function App() {
+
+  return <>
+  
+  <Hero></Hero>
+  </>;
+}
+
+export default App;
