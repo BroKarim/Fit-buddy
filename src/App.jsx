1eba4351283def96c612d73370f04175fd86fad0
Noitusanx <habilnasutionx@gmail.com>
2023-11-18 00:10:26 +0700

    feat: page gender option, height, weight

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Hero from './components/Hero';
 // import Header from './components/Header';
 import CardSection from './components/CardSection';
+import GenderOption from './components/GenderOption';
+import Height from './components/Height';
+import Weight from './components/Weight';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/gender" element={<GenderOption />} />
+        <Route path="/height" element={<Height />} />
+        <Route path="/weight" element={<Weight />} />
       </Routes>
     </Router>
   );
