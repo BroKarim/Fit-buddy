2b5be7e1fc6f503d09e3058e3030433c7e072338
kiramDZ <hilmi0@mhs.unsyiah.ac.id>
2023-11-17 21:24:30 +0700

    tambah data.js

---
+++ src/utils/data.js
@@ -0,0 +1,8 @@
+import adult from "../assets/image/adult.jpeg"
+import teenage from "../assets/image/teenage.jpg"
+import old_man from "../assets/image/old_man.jpg"
+
+
+export const oldCategory = {
+    adult, teenage, old_man
+}
