3986a0dc68076364ad607300082837f7552e9d1d
wakgeng <riyanfarhan86@gmail.com>
2023-11-23 17:14:39 +0700

    tambah tampilan visual workrecom

--- src/components/WorkRecom.jsx
+++ src/components/WorkRecom.jsx
@@ -12,7 +12,7 @@ export default function WorkRecom() {
         </div>
         <div className="flex md:flex-row flex-wrap start flex-col overflow-hidden  justify-items-start p-4 items-center">
           <a href="#" className="block py-4">
-            <iframe title="Giphy" src="https://giphy.com/embed/mGWBjz74fn404BWUl4" width="400" height="200" className="giphy-embed" allowFullScreen></iframe>
+            <iframe title="Giphy" src="https://giphy.com/embed/5t9IcXiBCyw60XPpGu" width="400" height="200" className="giphy-embed" allowFullScreen></iframe>
 
             <div className="mt-4 sm:flex sm:items-center sm:justify-center text-white sm:gap-4">
               <strong className="font-medium">Push Up</strong>
@@ -23,7 +23,7 @@ export default function WorkRecom() {
             </div>
           </a>
           <a href="#" className="block py-4">
-            <iframe title="Giphy" src="https://giphy.com/embed/xThuWcL1bfnd1MtZGE" width="400" height="200" frameBorder="0" className="giphy-embed rounded-md" allowFullScreen></iframe>
+            <iframe title="Giphy" src="https://giphy.com/embed/35EH7kLQiMM1YWYmUI" width="400" height="200" frameBorder="0" className="giphy-embed rounded-md" allowFullScreen></iframe>
 
             <div className="mt-4 sm:flex sm:items-center sm:justify-center text-white sm:gap-4">
               <strong className="font-medium">Sit up</strong>
@@ -34,21 +34,109 @@ export default function WorkRecom() {
             </div>
           </a>
           <a href="#" className="block py-4">
-            <iframe title="Giphy" src="https://giphy.com/embed/xThuWcL1bfnd1MtZGE" width="400" height="200" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+            <iframe title="Giphy" src="https://giphy.com/embed/1qfKN8Dt0CRdCRxz9q" width="400" height="200" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
 
             <div className="mt-4 sm:flex sm:items-center sm:justify-center text-white sm:gap-4">
-              <strong className="font-medium">Company Name</strong>
+              <strong className="font-medium">Squat</strong>
 
               <span className="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
 
-              <p className="mt-0.5 opacity-50 sm:mt-0">Branding / Signage</p>
+              <p className="mt-0.5 opacity-50 sm:mt-0">10 set of 12 reps</p>
             </div>
           </a>
           <a href="#" className="block py-4">
-            <iframe title="Giphy" src="https://giphy.com/embed/MxqKKnUJdSd3i" width="400" height="200" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+            <iframe title="Giphy" src="https://giphy.com/embed/2ALcpIRjQFi21eezSI" width="400" height="200" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
 
             <div className="mt-4 sm:flex sm:items-center sm:justify-center text-white sm:gap-4">
-              <strong className="font-medium">pull uos</strong>
+              <strong className="font-medium">Pull Up</strong>
+
+              <span className="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
+
+              <p className="mt-0.5 opacity-50 sm:mt-0">10 set of 12 reps</p>
+            </div>
+          </a>
+          <a href="#" className="block py-4">
+            <iframe title="Giphy" src="https://giphy.com/embed/1ncuQASTajJ0lx6Nix" width="400" height="200" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+
+            <div className="mt-4 sm:flex sm:items-center sm:justify-center text-white sm:gap-4">
+              <strong className="font-medium">Lunges</strong>
+
+              <span className="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
+
+              <p className="mt-0.5 opacity-50 sm:mt-0">10 set of 12 reps</p>
+            </div>
+          </a>
+          <a href="#" className="block py-4">
+            <iframe title="Giphy" src="https://giphy.com/embed/39wjDz1y3UI51qgv4K" width="400" height="200" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+
+            <div className="mt-4 sm:flex sm:items-center sm:justify-center text-white sm:gap-4">
+              <strong className="font-medium">Plank</strong>
+
+              <span className="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
+
+              <p className="mt-0.5 opacity-50 sm:mt-0">10 set of 12 reps</p>
+            </div>
+          </a>
+          <a href="#" className="block py-4">
+            <iframe title="Giphy" src="https://giphy.com/embed/2A5A9uJBzgyty" width="400" height="200" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+
+            <div className="mt-4 sm:flex sm:items-center sm:justify-center text-white sm:gap-4">
+              <strong className="font-medium">Crunches</strong>
+
+              <span className="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
+
+              <p className="mt-0.5 opacity-50 sm:mt-0">10 set of 12 reps</p>
+            </div>
+          </a>
+          <a href="#" className="block py-4">
+            <iframe title="Giphy" src="https://giphy.com/embed/ZcKxE91kiP7e3O7Me4" width="400" height="200" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+
+            <div className="mt-4 sm:flex sm:items-center sm:justify-center text-white sm:gap-4">
+              <strong className="font-medium">Jumping Jacks</strong>
+
+              <span className="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
+
+              <p className="mt-0.5 opacity-50 sm:mt-0">10 set of 12 reps</p>
+            </div>
+          </a>
+          <a href="#" className="block py-4">
+            <iframe title="Giphy" src="https://giphy.com/embed/5hqs5R78mB5thn3eSc" width="400" height="200" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+
+            <div className="mt-4 sm:flex sm:items-center sm:justify-center text-white sm:gap-4">
+              <strong className="font-medium">Sec Wall Sit</strong>
+
+              <span className="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
+
+              <p className="mt-0.5 opacity-50 sm:mt-0">10 set of 12 reps</p>
+            </div>
+          </a>
+          <a href="#" className="block py-4">
+            <iframe title="Giphy" src="https://giphy.com/embed/hp3PGxGMODVQnULnqz" width="400" height="200" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+
+            <div className="mt-4 sm:flex sm:items-center sm:justify-center text-white sm:gap-4">
+              <strong className="font-medium">Butt Kick</strong>
+
+              <span className="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
+
+              <p className="mt-0.5 opacity-50 sm:mt-0">10 set of 12 reps</p>
+            </div>
+          </a>
+          <a href="#" className="block py-4">
+            <iframe title="Giphy" src="https://giphy.com/embed/cM8tzmeTY0SYMz6VGj" width="400" height="200" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+
+            <div className="mt-4 sm:flex sm:items-center sm:justify-center text-white sm:gap-4">
+              <strong className="font-medium">Russian Twist</strong>
+
+              <span className="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
+
+              <p className="mt-0.5 opacity-50 sm:mt-0">10 set of 12 reps</p>
+            </div>
+          </a>
+          <a href="#" className="block py-4">
+            <iframe title="Giphy" src="https://giphy.com/embed/oohqcQSr77gXu" width="400" height="200" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+
+            <div className="mt-4 sm:flex sm:items-center sm:justify-center text-white sm:gap-4">
+              <strong className="font-medium">Leg Circles</strong>
 
               <span className="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
 
